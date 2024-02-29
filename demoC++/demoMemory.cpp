bc465f4ddee7d0571b691d31f8ee0e7ba69861ae
mzh <2426517790@qq.com>
2024-02-29 13:51:12 +0800

    [新增]string类型的demo

--- demoC++/demoMemory.cpp
+++ demoC++/demoMemory.cpp
@@ -2,31 +2,56 @@
 #include <cstdlib>
 #include <cstring>
 
+
+#if 0
 int main()
 {
-#if 0
+
     /* 一维数组开辟空间 */
     char* str = (char*)malloc(sizeof(char) * 10);
     if (str == NULL)
         return -1;
+    //str = "hello";//当你写下 str = "hello "; 时，实际上是将字符串常量 "hello " 的地址赋给了指针变量 str，而不是将 "hello " 这个字符串复制到 str 指向的内存中,这会导致两个问题：
+    /* 1.内存泄漏：在你调用 malloc 分配内存之后，将 "hello " 的地址赋给 str，原本分配的内存空间就无法访问到了，导致内存泄漏 */
+    /* 2.字符串常量不可更改：字符串常量是不可更改的，试图修改字符串常量所在的内存会导致未定义的行为 */
+    strcpy(str,"hello");
+    std::count<<str<<std::endl;
+    free(str);
+    rerturn 0;
 #endif
+
+#if 0
     char** str = (char**)malloc(sizeof(char*) * 3);
     if (str == NULL)
         return -1;
     for (int i = 0; i < 3; i++)
     {
-        char* str = (char*)malloc(sizeof(char) * 1);
-        strcpy(str[i], "hello");
-        std::cout<<str<<std::endl;
+        if (str[i] == NULL)
+        {
+            str[i] = (char*)malloc(sizeof(char) * 10);
+            if (str[i] == NULL)
+            {
+                return -1;
+            }
+            strcpy(str[i], "hello");
+        }
     }
-    
-    
+    for (int i = 0; i < 3; i++)
+    {
+        std::cout<<str[i]<<std::endl;
+    }
+    /* 释放空间 */
+    for (int i = 0; i < 3; i++)
+    {
+        free(str[i]);
+    }
+     
     free(str);
 
     return 0;
-    
+  
 }
-
+#endif
 int main()
 {
 #if 0
@@ -45,16 +70,36 @@ int main()
     std::cout<<*a<<std::endl;
     delete a;
 #endif
+
+#if 0
+    //char *s = (char*)malloc(sizeof(char) *10); //c
+    char **s = new char*[3]; //c++
+    //1.不需要计算内存大小
+    //2.不需要对指针进行转换
+    //3.不需要判断指针是否为空：抛出异常，终止程序
+    strcpy(s,"hello");
+    //释放数组
+    delete []s;
+#endif
+#if 1
+    int *a = new int(10); //小括号是赋值，[]申请数组
+    std::cout<<*a<<std::endl;
+    delete a;
     char **s = new char*[3];
     for (int i = 0; i < 3; i++)
     {
         s[i] = new char[10];
         strcpy(s[i], "hello");
-        std::cout<<
+        std::cout<<s[i]<<std::endl;
 
     }
+    for (int i = 0; i < 3; i++)
+    {
+        delete []s[i];
+    }
+    delete []s;
     
     return 0;
 
-
+#endif
 }
--- demoC++/demoString.cpp
+++ demoC++/demoString.cpp
@@ -4,11 +4,15 @@ int main()
 {
     //基于动态数组实现的，不需要担心越界问题
     std::string s;
-    s = "hello world";
-    std::string s1 = s + "world";
-    std::cout<<s1.size()<<std::endl;
-    std::cout<<s1.capacity()<<std::endl;
+    s = "hello world";//相当于strcpy，拷贝字符串
+    std::cout<<s<<std::endl;//直接输出，不需要去指针
+    std::string s1 = s + "world";//相当于strcat，
+    std::cout<<s1.size()<<std::endl;//计算字符串长度
+    std::cout<<s1.capacity()<<std::endl;//计算动态数组容积
 
+    s += "asd";
+    s.push_back('p');//在字符串后面追加单个字符，不需要担心越界，野指针等问题
+    std::cout<<s<<std::endl;
     if (s == s1)
     {
         std::cout<<"equal"<<std::endl;
@@ -17,7 +21,9 @@ int main()
     {
         std::cout<<"not equal"<<std::endl;
     }
-    std::cout<<s.substr(1,4)<<std::endl;
+    std::cout<<s.substr(1,4)<<std::endl;//截取指定的字符串
+    std::cout<<s.find("l")<<std::endl;
+    std::cout<<s.find_first_of("ello")<<std::endl;//查找第一个字符串匹配的位置
     
     return 0;
 }
