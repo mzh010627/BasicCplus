d90b8332801161f59dc896d1517c169ca8286d2e
mzh <2426517790@qq.com>
2024-03-01 21:12:46 +0800

    [新增]static关键字的demo

--- demoClass/demoStatic.cpp
+++ demoClass/demoStatic.cpp
@@ -1,10 +1,34 @@
 #include<iostream>
 class Test
 {
-private:
-    static int c;
+public:
+    Test()
+    {
+        count++;
+        std::cout<<"构造函数\n";
+    }
+    ~Test()
+    {
+        count--;
+        std::cout<<"析构函数\n";
+    }
+public:
+    // static int c;
+    static int count;
 };
+/* 静态成员变量的初始化 */
+int Test::count = 0;
 int main()
 {
+    //std::cout<<Test::c<<std::endl;
+    // Test t1;
+    // t1.c = 10;
+    // Test t2;
+    // std::cout<<t2.c<<std::endl;
+    Test *t1 = new Test();
+    Test *t2 = new Test();
+    delete t1 ;
+    std::cout<<Test::count<<std::endl;
+
     return 0;
 }
